065a1fb4555ac896d32b2d53c7d079838eab0f1b
Joseph JR1 <Joseph JR1@192.168.0.20>
2015-08-28 00:04:45 -0400

    INput

---
+++ src/main/java/ProjectY/player/KeyBoardInput.java
@@ -0,0 +1,28 @@
+package main.java.ProjectY.player;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardInput implements KeyListener  {
+	
+	private boolean[] keys = new boolean [120];
+	public boolean 
+	
+	
+	public void keyPressed(KeyEvent arg0) {
+		
+		
+	}
+
+
+	public void keyReleased(KeyEvent arg0) {
+		
+		
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+	
+		
+	}
+
+}
