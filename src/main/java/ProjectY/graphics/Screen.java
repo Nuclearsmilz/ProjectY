400e63375c7b176f5b6da69229c82285a2b252c1
Joseph JR1 <Joseph JR1@192.168.0.20>
2015-08-28 13:29:42 -0400

    The Screen

--- src/main/java/ProjectY/graphics/Screen.java
+++ src/main/java/ProjectY/graphics/Screen.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Screen {
 	
 	private int width, height;
+	int x,y;
 	public int[] pixels;
 	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
@@ -19,7 +20,7 @@ public class Screen {
 		pixels = new int[width * height];
 		
 		for (int i = 0; i < 32 * 32; i++) {
-			tiles[i] = random.nextInt(0xffffff);
+			tiles[i] = random.nextInt(0x9900CC);
 		}
 	}
 	
@@ -32,9 +33,9 @@ public class Screen {
 	public void render() {
 		
 		for (int y = 0; y < height; y++) {
-			
+			if (y < 0 || y>= height) break;			
 			for (int x = 0; x < width; x++) {
-			
+				if (x < 0 || s  x >= width) break;
 			}
 		}
 	}
