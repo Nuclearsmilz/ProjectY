41220a76b563fb58de359600fa7863834c4abbb4
Jon Marien-Toshiba <nuclearsmilz@gmail.com>
2015-08-27 23:21:46 -0400

    Game + Screen

--- src/main/java/ProjectY/ProjectY.java
+++ src/main/java/ProjectY/ProjectY.java
@@ -12,119 +12,105 @@ import javax.swing.JFrame;
 import main.java.ProjectY.graphics.Screen;
 
 public class ProjectY extends Canvas implements Runnable {
-
-/**
-	 * 
-	 */
-	//The Size Of The Window
+	
 	private static final long serialVersionUID = 1L;
-public static int width = 300;
-public static int height = width / 16 *9 ;
-public static int scale = 3;
-
-public static String title = "ProjectY";
-
-private Thread thread;
-private JFrame frame;
-private boolean running = false;
-
-
-
-private Screen screen;
-
-private BufferedImage Image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-private int [] pixels =  ((DataBufferInt) Image.getRaster().getDataBuffer()).getData();
-
-public ProjectY () {
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
 	
-	Dimension size = new Dimension (width*scale,height*scale);
-	setPreferredSize(size);
+	public static String title = "ProjectY";
 	
-	screen =new Screen(width, height);
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
 	
+	private Screen screen;
 	
+	private BufferedImage Image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) Image.getRaster().getDataBuffer()).getData();
 	
-	frame = new JFrame();
-}
-
-//Running State
-public synchronized void start() {
-	running = true;
-	thread = new Thread (this, "Display");
-	thread.start();
+	public ProjectY() {
+		
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		screen = new Screen(width, height);
+		
+		frame = new JFrame();
+	}
 	
-	}	
-//Stopped State
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
 	public synchronized void stop() {
 		running = false;
 		try {
-		thread.join();
-	}	catch (InterruptedException e) {
-		e.printStackTrace();
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	//While Running Loops
 	public void run() {
 		long lastTime = System.nanoTime();
 		long timer = System.currentTimeMillis();
-		final double ns = 1000000000.0 / 60.0 ;
+		final double ns = 1000000000.0 / 60.0;
 		double delta = 0;
 		int frames = 0;
 		int updates = 0;
 		
-		
 		while (running) {
-				long now = System.nanoTime();
-			delta += (now - lastTime) / ns ;
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while (delta>= 1) {
+			while (delta >= 1) {
 				update();
 				updates++;
 				delta--;
 			}
-		
+			
 			render();
 			frames++;
 			
 			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
 				System.out.println(updates + "ups," + frames + "fps");
-				frame.setTitle(title + "  |  " + updates + "ups," + frames + "fps" );
+				frame.setTitle(title + "  |  " + updates + "ups," + frames + "fps");
 				frames = 0;
 				updates = 0;
 			}
 		}
-		
 	}
 	
 	public void update() {
-		
+	
 	}
+	
 	//Buffering
 	public void render() {
-		BufferStrategy bs = getBufferStrategy() ;
-		if(bs == null) {
-			createBufferStrategy(3) ;
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
 			return;
 			
 		}
-		screen.Clear();
+		screen.clear();
 		screen.render();
 		
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
-				
-				
+		
 		Graphics g = bs.getDrawGraphics();
 		g.drawImage(Image, 0, 0, getWidth(), getHeight(), null);
-		g.dispose();		
+		g.dispose();
 		bs.show();
 	}
-	// Main String 
-	public static void main (String[]  args) {
-		
+	
+	public static void main(String[] args) {
 		
 		ProjectY game = new ProjectY();
 		game.frame.setResizable(false);
@@ -134,8 +120,7 @@ public synchronized void start() {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-
-		game.start();
 		
+		game.start();
 	}
 }
