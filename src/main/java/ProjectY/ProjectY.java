93c70cb7c44bbd33039506bc55c552850315ada3
Jon Marien-Toshiba <nuclearsmilz@gmail.com>
2015-08-27 23:23:10 -0400

    Black screen, but runs now

--- src/main/java/ProjectY/ProjectY.java
+++ src/main/java/ProjectY/ProjectY.java
@@ -12,8 +12,8 @@ import javax.swing.JFrame;
 import main.java.ProjectY.graphics.Screen;
 
 public class ProjectY extends Canvas implements Runnable {
-	
 	private static final long serialVersionUID = 1L;
+	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
@@ -89,7 +89,6 @@ public class ProjectY extends Canvas implements Runnable {
 	
 	}
 	
-	//Buffering
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
 		if (bs == null) {
